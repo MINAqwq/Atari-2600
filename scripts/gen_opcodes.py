3aecf89083d83813b0ae6c7a951ea3e162febf57
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-02 22:59:12 +0100

    opcode generator script

---
+++ scripts/gen_opcodes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from sys import argv, stdout
+
+FILE_PATH: str = "src/cpu/opcodes.h"
+
+lines: list[str] = []
+opcodes: list[str] = []
+
+try:
+        with open(FILE_PATH, "r") as f:
+                lines = [line.rstrip() for line in f]
+except Exception as e:
+        print("run script from repository root :)")
+
+
+
+for line in lines:
+        if "void OP_" not in line:
+                continue
+
+        opcodes.append(line[8] + line[9])
+
+stdout.write("op_ptr\nget_opcode(uint8_t op)\n{\n\tswitch(op) {\n")
+for op in opcodes:
+        stdout.write("\tcase 0x" + op + ":\n\t\treturn &OP_" + op + ";\n")
+
+stdout.write("\t}\n\treturn NULL;\n}\n")
