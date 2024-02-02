4229ccfb84d30ee2d678bf110fe9613f626a2bfb
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-02 21:46:11 +0100

    added fake rom

--- src/atari6200.c
+++ src/atari6200.c
@@ -2,6 +2,22 @@
 #include "cpu/6507.h"
 #include "utils/debug.h"
 
+/* fake rom:
+ * LDX #00
+ * LDY #55
+ *
+ * LDA #05
+ * STA $10
+ *
+ * loop:
+ * STY $11,X
+ * INX
+ * CPX $10
+ * BNE loop
+ */
+static uint8_t rom[] = {0xA2, 0x00, 0xA0, 0x37, 0xA9, 0x05, 0x06, 0x85,
+                        0x10, 0x94, 0x11, 0xE8, 0xE4, 0x10, 0xD0, 0xF9};
+
 int
 main()
 {
@@ -10,15 +26,12 @@ main()
 
 	c6507_init(&bus, &cpu);
 
-	/* fake rom */
-	bus_write(0x05, 0x03, &bus);
-	bus_write(0x06, 0x05, &bus);
-	bus_write(0x80, 0x65, &bus);
-	bus_write(0x81, 0x06, &bus);
-
 	cpu.regs.a = 5;
-	cpu.regs.pc = 0x80;
+	cpu.regs.pc = 0x1000;
 	cpu.cycle_count = 0;
+
+	/* load fake rom */
+	bus.memory_map.rom = &rom[0];
 
 	c6507_clock(&cpu);
 
