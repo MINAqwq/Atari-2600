f8ab860450ff2dcafbf1000fabbbeee9203085bc
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-03 18:35:49 +0100

    omg

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -32,7 +32,7 @@ debug_print_cpu_status(const C6507 *c)
 	       "NF:\t%d\n"
 	       "PC:\t%d\n\n"
 	       "Cycle Count: %d\n",
-	       c->regs.a, c->regs.y, c->regs.x, c->regs.s, c->regs.p.carry,
+	       c->regs.a, c->regs.x, c->regs.y, c->regs.s, c->regs.p.carry,
 	       c->regs.p.zero, c->regs.p.irq_disable, c->regs.p.decimal_mode,
 	       c->regs.p.overflow, c->regs.p.negative, c->regs.pc,
 	       c->cycle_count);
