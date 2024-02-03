4335713b7c31ab2b50b74069703c397b289e53bf
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-03 20:37:00 +0100

    better debugging stuff episode 2

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -20,17 +20,17 @@ void
 debug_print_cpu_status(const C6507 *c)
 {
 	printf("------ CPU Status ------\n"
-	       "A:\t%d\n"
-	       "X:\t%d\n"
-	       "Y:\t%d\n"
-	       "SP:\t%d\n"
+	       "A:\t%02X\n"
+	       "X:\t%02X\n"
+	       "Y:\t%02X\n"
+	       "SP:\t%02X\n"
 	       "CF:\t%d\n"
 	       "ZF:\t%d\n"
 	       "ID:\t%d\n"
 	       "DM:\t%d\n"
 	       "VF:\t%d\n"
 	       "NF:\t%d\n"
-	       "PC:\t%d\n\n"
+	       "PC:\t%02X\n\n"
 	       "Cycle Count: %d\n",
 	       c->regs.a, c->regs.x, c->regs.y, c->regs.s, c->regs.p.carry,
 	       c->regs.p.zero, c->regs.p.irq_disable, c->regs.p.decimal_mode,
