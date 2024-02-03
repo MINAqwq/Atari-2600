ac5699eaaaec48289f0151ebd63ecfb6b40a40bd
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-03 20:34:15 +0100

    better memory logs

--- src/bus/bus.c
+++ src/bus/bus.c
@@ -9,7 +9,7 @@
 /* print error message and terminate process */
 void __attribute__((__noreturn__)) access_violation(unsigned int addr)
 {
-	fprintf(stderr, "access violation on addr $%02x\n", addr);
+	fprintf(stderr, "access violation on addr $%04X\n", addr);
 	exit(1);
 }
 
@@ -25,14 +25,14 @@ bus_write(uint16_t addr, uint8_t val, Bus *b)
 {
 	uint8_t *ptr;
 
-	debug_log("MEM: write #%d to $%04x", val, addr);
-
 	if (addr > MAX_ADDR)
 		access_violation(addr);
 
 	ptr = memory_map_get(addr, &b->memory_map);
 	if (!ptr)
 		access_violation(addr);
+
+	debug_log("MEM: write 0x%02X to $%04X", val, addr);
 
 	*ptr = val;
 }
@@ -42,14 +42,14 @@ bus_read(uint16_t addr, Bus *b)
 {
 	const uint8_t *ptr;
 
-	debug_log("MEM: read from %04x", addr);
-
 	if (addr > MAX_ADDR)
 		access_violation(addr);
 
 	ptr = memory_map_get(addr, &b->memory_map);
 	if (!ptr)
 		access_violation(addr);
+
+	debug_log("MEM: read 0x%02X from $%04X", *ptr, addr);
 
 	return *ptr;
 }
