5fe5db97240102f397656be90fe950af575acdac
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-02-04 04:25:20 +0100

    added -p option

--- scripts/gen_opcodes.py
+++ scripts/gen_opcodes.py
@@ -13,13 +13,16 @@ try:
 except Exception as e:
         print("run script from repository root :)")
 
-
-
 for line in lines:
         if "void OP_" not in line:
                 continue
 
         opcodes.append(line[8] + line[9])
+
+# -p
+if len(argv) > 1 and argv[1] == "-p":
+        stdout.write(f"{((len(opcodes) / 0x100) * 100):.2f}% (including illegal opcodes)\n")
+        exit(0)
 
 stdout.write("op_ptr\nget_opcode(uint8_t op)\n{\n\tswitch(op) {\n")
 for op in opcodes:
